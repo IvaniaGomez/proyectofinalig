3e628ed03f2e18811d956715be598bf320376fdb
IvaniaGomez <2021-40392@liceocanadiense.edu.gt>
2021-08-25 12:43:03 -0600

    subiendo el proyecto

---
+++ info.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="ima/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="diseño.css">
+    <title>Artesanías</title>
+</head>
+<body>
+    <!doctype html>
+<html lang="en">
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
+    <title>Artesanías</title>
+  </head>
+  <body>
+    <img src="ima/portada.jpg" width="1519px">
+      <div class="btn-group w-100" role="group" aria-label="Basic example">
+  <button type="button" class="btn btn-warning"><a href="productos.html" style="text-decoration: none; " >Productos / Servicios</a></button>
+  <button type="button" class="btn btn-warning"><a href="tienda.html" style="text-decoration: none;" >Tiendas</a></button>
+  <button type="button" class="btn btn-warning"><a href="contactenos.html" style="text-decoration: none;" >Contactenos</a></button>
+</div>
+<br><br><br>
+<br><br>
+<div class="col-3">
+<br><h5>Artesanías Guatemaltecas</h5> 
